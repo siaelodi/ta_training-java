9c93c4668b16ab0d9f13f9c6cf78fffd9d665674
Alex Nem <61789909+siaelodi@users.noreply.github.com>
2022-04-01 00:12:11 +0600

    reverse update

--- src/com/epam/training/student_alexandra_nemtseva/fundamentals/ReverseArgs.java
+++ src/com/epam/training/student_alexandra_nemtseva/fundamentals/ReverseArgs.java
@@ -5,8 +5,9 @@ package com.epam.training.student_alexandra_nemtseva.fundamentals;
  */
 public class ReverseArgs {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++){
-            System.out.println(String.format("Argument %d: %s", i, args[i]));
+
+        for( int i = args.length - 1 ; i >= 0 ; i--) {
+            System.out.println("Reverse order: " + args[i]);
         }
     }
 }
