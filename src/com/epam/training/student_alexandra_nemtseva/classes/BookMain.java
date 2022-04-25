c717a4aa1af6ef0af7b6768d8b80985470d454fe
Alex Nem <61789909+siaelodi@users.noreply.github.com>
2022-04-25 08:40:48 +0600

    test push to second branch

---
+++ src/com/epam/training/student_alexandra_nemtseva/classes/BookMain.java
@@ -0,0 +1,15 @@
+package com.epam.training.student_alexandra_nemtseva.classes;
+
+/**
+ * Задать критерий выбора данных и вывести эти данные на консоль.
+ *
+ * Создать массив объектов. Вывести:
+ * a) список книг заданного автора;
+ * b)   список книг, выпущенных заданным издательством;
+ * c)   список книг, выпущенных после заданного года.
+ */
+public class BookMain {
+    public static void main(String[] args) {
+        Book fantasy = new Book();
+    }
+}
